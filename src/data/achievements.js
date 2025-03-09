657587462dfa7fedb41ff9d4095f22dd5dc2b67e
fikrilal <fikrimuzakky24@gmail.com>
2025-03-09 10:26:35 +0700

    feat: add certificate credential

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -5,33 +5,33 @@ const achievements = [
     organization:
       "Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi (Kemdikbudristek)",
     date: "September 2023",
-    credentialLink: "#",
+    credentialLink: "https://bit.ly/pkm-certificate",
   },
   {
     title: "3rd Runner Up Musabaqah Design Aplikasi Al-Qur'an (MDAQ) 2023",
     organization: "Universitas Negeri Yogyakarta",
     date: "April 2023",
-    credentialLink: "#",
+    credentialLink: "https://bit.ly/mdaq-certificate",
   },
   {
     title: "2nd Place in UI/UX Design at UI/UX Design Competition 2022",
     organization: "HMPS-TI UNISBANK",
     date: "September 2022",
-    credentialLink: "#",
+    credentialLink: "https://bit.ly/uxd-comp-certificate",
   },
   {
     title:
       "1st Place in UI/UX Design at Informatic's Conference & Competition (INFECT) 2022",
     organization: "HMTI Universitas Muhammadiyah Purwokerto",
     date: "July 2022",
-    credentialLink: "#",
+    credentialLink: "https://bit.ly/infect-2022-certificate",
   },
   {
     title:
       "1st Place in UI/UX Design at Information Technology Competition (ITC) 2022",
     organization: "HMJTI Politeknik Negeri Jember",
     date: "June 2022",
-    credentialLink: "#",
+    credentialLink: "https://bit.ly/1st-ux-hmjti-certificate",
   },
 ];
 
