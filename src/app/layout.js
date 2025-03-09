c1ea3c83d9eeb631e4600c57fc550f43b27b183e
fikrilal <fikrimuzakky24@gmail.com>
2025-03-09 11:55:58 +0700

    feat: disable zoom in mobile device

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body
         className={`${interTight.variable} ${fragmentMono.variable} font-sans antialiased dark:bg-darkbg dark:text-white`}
       >
